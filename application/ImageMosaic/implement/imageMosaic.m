af37fffb09718d77dd7ceafcc1ec2e80b8e003de
jxlixiong@163.com <921690764@qq.com>
2022-04-06 23:33:01 +0800

    add stitch dataset

---
+++ application/ImageMosaic/implement/imageMosaic.m
@@ -0,0 +1,19 @@
+%% 图像拼接的实现：
+clear;
+clc;
+
+%% 读取图片
+file1='src/dataset1/000/left.png';  
+file2='src/dataset1/000/right.png';  
+I1=imread(file1); % 读取图片
+I2=imread(file2);
+
+%% 图像预处理
+
+
+%% 图像配准
+
+
+%% 图像融合
+
+
